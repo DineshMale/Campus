e148e19264faf8e250ae47dfaffc785a7f30d199
Mudavath Manisha <mudavathmanisha76@gmail.com>
2026-01-07 11:30:20 +0530

    Updated OCR app.py and added requirements.txt

--- campusbuddyocr/app.py
+++ campusbuddyocr/app.py
@@ -192,4 +192,5 @@ def seating_ocr():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
